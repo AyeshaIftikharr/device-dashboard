70a04951e66951f722f7277cbccdc3fca7a54a0b
Ayesha Iftikhar <ayesha.ifr@gmail.com>
2020-09-20 18:05:20 +0500

    :see_no_evil: Add gitignore file and prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  semi: true,
+  tabWidth: 2,
+};
