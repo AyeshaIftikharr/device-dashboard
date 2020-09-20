ff2ec606f21a06c14b614ad027425aaeec0e90e9
Ayesha Iftikhar <ayesha.ifr@gmail.com>
2020-09-20 19:12:39 +0500

    :tada: Finish up basic application setup

--- src/index.js
+++ src/index.js
@@ -2,23 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 
-import styles from './styles.css';
 import App from './App';
 
 function render(Component) {
-    ReactDOM.render(
-        <AppContainer>
-            <Component/>
-        </AppContainer>,
-        document.getElementById('root')
-    );
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root'),
+  );
 }
 
 render(App);
 
 if (module.hot) {
-    module.hot.accept('./App', () => {
-        const NextApp = require('./App').default;
-        render(NextApp);
-    });
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    render(NextApp);
+  });
 }
